d4f480861579521f33f9120848e0e59f80714ac4
liuxidejia <rh1756075103@163.com>
2019-10-12 14:10:54 +0800

    这是第一版

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp=require("gulp");
+// const sass=require("gulp-sass")
+
+//复制项目下的所有代码复制到服务器下的发布目录下
+    gulp.task('copyfile',async ()=>{
+        gulp.src('./**/*')
+        .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu'));
+    })
+
+
+// // 执行监听sass任务
+// gulp.task('watchall',async ()=>{
+//     //监听scss
+//     gulp.watch('sass/**/*',async ()=>{
+//         gulp.src('sass/**/*')
+//         .pipe(sass())
+//         .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\css'));
+//     });
+//    //监听demo
+//     gulp.watch('demo/**/*',async ()=>{
+//         gulp.src('demo/**/*')
+//         .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\demo'));
+//     });
+//     //监听XX
+    
+// });
+
