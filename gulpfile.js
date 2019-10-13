da951f73432efb0e47006d5477885efcb56ba265
liuxidejia <rh1756075103@163.com>
2019-10-13 15:30:50 +0800

    这是第二版

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp=require("gulp");
-// const sass=require("gulp-sass")
+const sass=require("gulp-sass")
 
 //复制项目下的所有代码复制到服务器下的发布目录下
     gulp.task('copyfile',async ()=>{
@@ -8,20 +8,20 @@ const gulp=require("gulp");
     })
 
 
-// // 执行监听sass任务
-// gulp.task('watchall',async ()=>{
-//     //监听scss
-//     gulp.watch('sass/**/*',async ()=>{
-//         gulp.src('sass/**/*')
-//         .pipe(sass())
-//         .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\css'));
-//     });
-//    //监听demo
-//     gulp.watch('demo/**/*',async ()=>{
-//         gulp.src('demo/**/*')
-//         .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\demo'));
-//     });
-//     //监听XX
+//执行监听sass任务
+gulp.task('watchall',async ()=>{
+    //监听scss
+    gulp.watch('sass/**/*',async ()=>{
+        gulp.src('sass/**/*')
+        .pipe(sass())
+        .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\css'));
+    });
+   //监听demo
+    gulp.watch('demo/**/*',async ()=>{
+        gulp.src('demo/**/*')
+        .pipe(gulp.dest('D:\\phpStudy\\WWW\\xiangmu\\demo'));
+    });
+    //监听XX
     
-// });
+});
 
